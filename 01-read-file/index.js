caf78c2e097a4010c3cb6b546da15033ed376c9f
Timofey <naryshkint4@gmail.com>
2025-01-17 22:58:53 +0400

    feat: finish task 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,9 @@
+const fs = require('node:fs');
+const path = require('node:path');
+
+const stream = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
+let data = '';
+
+stream.on('data', (chunk) => (data += chunk));
+stream.on('end', () => console.log(data));
+stream.on('error', (error) => console.log('Error', error.message));
