b174434b2d60a943a81e342fe805a8df6e0ed3df
Timofey <naryshkint4@gmail.com>
2025-01-18 03:32:41 +0400

    feat: finish task 05-merge-styles

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,31 @@
+const fs = require('node:fs');
+const path = require('node:path');
+
+const bundlePath = path.join(__dirname, 'project-dist', 'bundle.css');
+const stylesDir = path.join(__dirname, 'styles');
+const stylesArray = [];
+
+fs.readdir(stylesDir, { withFileTypes: true }, (err, files) => {
+  const cssFiles = files.filter(
+    (file) => file.isFile() && path.extname(file.name) === '.css',
+  );
+  let filesProcessed = 0;
+  cssFiles.forEach((file) => {
+    if (err) console.log(err);
+    const filePath = path.join(stylesDir, file.name);
+    let fileData = '';
+    const stream = fs.createReadStream(filePath, 'utf-8');
+    stream.on('data', (chunk) => {
+      fileData += chunk;
+    });
+    stream.on('end', () => {
+      stylesArray.push(fileData);
+      if (filesProcessed === cssFiles.length) {
+        fs.writeFile(bundlePath, stylesArray.join('\n'), (err) => {
+          if (err) throw err;
+        });
+      }
+    });
+    filesProcessed++;
+  });
+});
